f63d158f70d18f01caa65c7f499b403f7f899238
Saimall <rameshwaramsaimallik@gmail.com>
2023-09-06 00:27:56 +0530

    Updated UI

--- src/components/Favourites/index.tsx
+++ src/components/Favourites/index.tsx
@@ -7,7 +7,6 @@ const Favouritesdisplay=()=>{
 
     return (
       <div className='m-2 bg-gray-200'>
-      <h1 className="py-3 px-6 font-bold text-2xl">Favourites</h1>
       <div className='m-2'>
         <Favourites/>
         </div>
