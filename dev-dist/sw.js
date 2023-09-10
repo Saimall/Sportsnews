cd01354a3f3c07b4090587ef2325ebaa2fa5c947
Saimall <rameshwaramsaimallik@gmail.com>
2023-09-10 11:32:45 +0530

    Updated  automatically

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -88,7 +88,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8a41kdecvmo"
+    "revision": "0.42q8821tpk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
