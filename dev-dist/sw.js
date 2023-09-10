8358adb4e9ec804ea529ad5392fcfaa62ab10bbc
Saimall <rameshwaramsaimallik@gmail.com>
2023-09-10 12:11:56 +0530

    Added demo video link

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -88,7 +88,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.42q8821tpk"
+    "revision": "0.cji4ssd4jeo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
